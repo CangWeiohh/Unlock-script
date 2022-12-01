e40c54e9cff1212e2085f993712a55c48a9c7102
CangWei <96470348+CangWeiohh@users.noreply.github.com>
2022-12-01 11:59:04 +0800

    Update Fileball.js

--- qx/Fileball.js
+++ qx/Fileball.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body fileball.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/CangWeiohh/Script/main/revenuecat.js
 
 [mitm] 
 
